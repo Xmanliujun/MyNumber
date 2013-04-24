26922dc77945de564301704080a68cb64776f771
Xmanliujun <644240319@qq.com>
2013-04-24 10:06:27 +0800

    Initial Commit

---
+++ MyNumber/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyNumber
+//
+//  Created by XmL on 13-4-24.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyNumber/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyNumber
+//
+//  Created by XmL on 13-4-24.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
